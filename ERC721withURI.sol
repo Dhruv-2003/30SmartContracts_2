a4145e5decdbfdd20b8d61729e4b17a4bfe974d2
dhruv <dhruvagarwal2017@gmail.com>
2022-10-02 11:44:52 +0530

    ERC721 with URI

--- ERC721withURI.sol
+++ ERC721withURI.sol
@@ -1,47 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.4;
 
-// ERC721
-// - A ERC721 Token with a mint method
-// - Openzepplin library can be used
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-// Import the openzepplin contracts
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC721/ERC721.sol";
+contract MyToken is ERC721, ERC721URIStorage, Ownable {
+    constructor() ERC721("MyToken", "MTK") {}
 
-/// import the Supply contract for ERC721 that enables saving the
-
-// MyNFT is  ERC721 signifies that the contract we are creating imports ERC721 and follows ERC721 contract from openzeppelin
-contract myNFT is ERC721 {
-    /// assigning the rate & supply
-    uint256 rate = 0.01 ether;
-    uint256 supply = 10000;
-
-    uint256 minted = 0;
-
-    constructor() ERC721("MyNFT", "MN") {
-        // mint an NFT to yourself
-        _mint(msg.sender, 1);
+    function safeMint(
+        address to,
+        uint256 tokenId,
+        string memory uri
+    ) public onlyOwner {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
 
-    // to mint the NFT
-    function mint() public payable {
-        require(minted + 1 <= supply, "Exceeded maximum NFT supply");
-        require(msg.value >= rate, "Not Enough Ether");
-        _mint(msg.sender, 1);
-        minted += 1;
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
     }
 
-    // to withdraw the amount by the owner
-    function withdraw() public onlyOwner {
-        address _owner = owner();
-        uint256 amount = address(this).balance;
-        (bool sent, ) = _owner.call{value: amount}("");
-        require(sent, "Failed to send Ether");
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
-
-    // Function to receive Ether. msg.data must be empty
-    receive() external payable {}
-
-    // Fallback function is called when msg.data is not empty
-    fallback() external payable {}
 }
