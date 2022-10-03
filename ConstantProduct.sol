553eab59645d0e14ec376b8416c5a0c4f716c5b7
dhruv <dhruvagarwal2017@gmail.com>
2022-10-03 19:40:15 +0530

    Constant Product completed

--- ConstantProduct.sol
+++ ConstantProduct.sol
@@ -6,44 +6,56 @@ contract CPAMM {
     IERC20 public immutable token0;
     IERC20 public immutable token1;
 
-    // tracking the supply of both the tokens in the pool
+    // tracks the internal balance of both the tokens
     uint256 public reserve0;
     uint256 public reserve1;
 
+    // tracks the total share minted
     uint256 public totalSupply;
+    // track individual share minted
     mapping(address => uint256) public balanceOf;
 
     constructor(address _token0, address _token1) {
+        // token intialisation
         token0 = IERC20(_token0);
         token1 = IERC20(_token1);
     }
 
+    // to mint some shares
     function _mint(address _to, uint256 _amount) private {
         balanceOf[_to] += _amount;
         totalSupply += _amount;
     }
 
+    // to burn the shares
     function _burn(address _from, uint256 _amount) private {
         balanceOf[_from] -= _amount;
         totalSupply -= _amount;
     }
 
+    // to update the reserve of both the tokens of this contract
     function _update(uint256 _reserve0, uint256 _reserve1) private {
         reserve0 = _reserve0;
         reserve1 = _reserve1;
     }
 
+    // swap 2 tokens which are in AMM
     function swap(address _tokenIn, uint256 _amountIn)
         external
         returns (uint256 amountOut)
     {
+        // the tokens can just be token0  or token1
         require(
             _tokenIn == address(token0) || _tokenIn == address(token1),
             "invalid token"
         );
+
         require(_amountIn > 0, "amount in = 0");
 
+        /// checking if the token is token0
         bool isToken0 = _tokenIn == address(token0);
+
+        /// assigning the tokens on the basis of bool to be able to call some functions
         (
             IERC20 tokenIn,
             IERC20 tokenOut,
@@ -53,27 +65,27 @@ contract CPAMM {
                 ? (token0, token1, reserve0, reserve1)
                 : (token1, token0, reserve1, reserve0);
 
+        /// transferring the tokens from sender to this contract
         tokenIn.transferFrom(msg.sender, address(this), _amountIn);
 
-        /*
-        How much dy for dx?
-
-        xy = k
-        (x + dx)(y - dy) = k
-        y - dy = k / (x + dx)
-        y - k / (x + dx) = dy
-        y - xy / (x + dx) = dy
-        (yx + ydx - xy) / (x + dx) = dy
-        ydx / (x + dx) = dy
-        */
         // 0.3% fee
         uint256 amountInWithFee = (_amountIn * 997) / 1000;
+
+        /*
+        ydx / (x + dx) = dy
+        */
+        // y = reserveOut , dy = amountOut
+        // x = reserveIn , dx = amountInWithFee
         amountOut =
             (reserveOut * amountInWithFee) /
             (reserveIn + amountInWithFee);
 
+        /// tokensOut transferred to the sender
         tokenOut.transfer(msg.sender, amountOut);
 
+        // swap complete
+
+        // updating the reserve of the contract
         _update(
             token0.balanceOf(address(this)),
             token1.balanceOf(address(this))
@@ -84,23 +96,14 @@ contract CPAMM {
         external
         returns (uint256 shares)
     {
+        /// transferring the tokens to the contract first
         token0.transferFrom(msg.sender, address(this), _amount0);
         token1.transferFrom(msg.sender, address(this), _amount1);
 
         /*
-        How much dx, dy to add?
-
-        xy = k
-        (x + dx)(y + dy) = k'
-
-        No price change, before and after adding liquidity
         x / y = (x + dx) / (y + dy)
-
-        x(y + dy) = y(x + dx)
         x * dy = y * dx
-
         x / y = dx / dy
-        dy = y / x * dx
         */
         if (reserve0 > 0 || reserve1 > 0) {
             require(
@@ -112,8 +115,7 @@ contract CPAMM {
         /*
         How much shares to mint?
 
-        f(x, y) = value of liquidity
-        We will define f(x, y) = sqrt(xy)
+        f(x, y) = value of liquidity  = sqrt(xy)
 
         L0 = f(x, y)
         L1 = f(x + dx, y + dy)
@@ -122,54 +124,32 @@ contract CPAMM {
 
         Total shares should increase proportional to increase in liquidity
         L1 / L0 = (T + s) / T
+        (L1 - L0)*T / L0 = s 
 
-        L1 * T = L0 * (T + s)
 
-        (L1 - L0) * T / L0 = s 
-        */
-
-        /*
-        Claim
+        on solving ,we get the result as below
         (L1 - L0) / L0 = dx / x = dy / y
 
-        Proof
-        --- Equation 1 ---
-        (L1 - L0) / L0 = (sqrt((x + dx)(y + dy)) - sqrt(xy)) / sqrt(xy)
-        
-        dx / dy = x / y so replace dy = dx * y / x
-
-        --- Equation 2 ---
-        Equation 1 = (sqrt(xy + 2ydx + dx^2 * y / x) - sqrt(xy)) / sqrt(xy)
-
-        Multiply by sqrt(x) / sqrt(x)
-        Equation 2 = (sqrt(x^2y + 2xydx + dx^2 * y) - sqrt(x^2y)) / sqrt(x^2y)
-                   = (sqrt(y)(sqrt(x^2 + 2xdx + dx^2) - sqrt(x^2)) / (sqrt(y)sqrt(x^2))
-        
-        sqrt(y) on top and bottom cancels out
-
-        --- Equation 3 ---
-        Equation 2 = (sqrt(x^2 + 2xdx + dx^2) - sqrt(x^2)) / (sqrt(x^2)
-        = (sqrt((x + dx)^2) - sqrt(x^2)) / sqrt(x^2)  
-        = ((x + dx) - x) / x
-        = dx / x
-
-        Since dx / dy = x / y,
-        dx / x = dy / y
-
-        Finally
-        (L1 - L0) / L0 = dx / x = dy / y
+        s = dx * T / x = dy * T / y
         */
+
         if (totalSupply == 0) {
+            /// then S = T = sqrt(XY)
             shares = _sqrt(_amount0 * _amount1);
         } else {
+            /// whichever value is minimum will be the amount of shares of (dx * T / x ) or (dy * T / y)
             shares = _min(
                 (_amount0 * totalSupply) / reserve0,
                 (_amount1 * totalSupply) / reserve1
             );
         }
+
         require(shares > 0, "shares = 0");
+
+        /// mint the shares for the user
         _mint(msg.sender, shares);
 
+        /// update the reserve of the tokens again
         _update(
             token0.balanceOf(address(this)),
             token1.balanceOf(address(this))
@@ -181,55 +161,47 @@ contract CPAMM {
         returns (uint256 amount0, uint256 amount1)
     {
         /*
-        Claim
         dx, dy = amount of liquidity to remove
         dx = s / T * x
         dy = s / T * y
-
-        Proof
-        Let's find dx, dy such that
-        v / L = s / T
         
+
+        Calculation :
+
         where
         v = f(dx, dy) = sqrt(dxdy)
         L = total liquidity = sqrt(xy)
         s = shares
         T = total supply
 
-        --- Equation 1 ---
         v = s / T * L
-        sqrt(dxdy) = s / T * sqrt(xy)
-
-        Amount of liquidity to remove must not change price so 
-        dx / dy = x / y
-
-        replace dy = dx * y / x
-        sqrt(dxdy) = sqrt(dx * dx * y / x) = dx * sqrt(y / x)
-
-        Divide both sides of Equation 1 with sqrt(y / x)
-        dx = s / T * sqrt(xy) / sqrt(y / x)
-           = s / T * sqrt(x^2) = s / T * x
-
         Likewise
         dy = s / T * y
+        dx = s / T * x
+
         */
 
+        // fetching the current balance from the contract
         // bal0 >= reserve0
         // bal1 >= reserve1
         uint256 bal0 = token0.balanceOf(address(this));
         uint256 bal1 = token1.balanceOf(address(this));
 
+        /// calculating the amounts of both the tokens
         amount0 = (_shares * bal0) / totalSupply;
         amount1 = (_shares * bal1) / totalSupply;
         require(amount0 > 0 && amount1 > 0, "amount0 or amount1 = 0");
 
+        /// burn the shares and update the reserve
         _burn(msg.sender, _shares);
         _update(bal0 - amount0, bal1 - amount1);
 
+        /// now transferring the tokens back to the user
         token0.transfer(msg.sender, amount0);
         token1.transfer(msg.sender, amount1);
     }
 
+    /// function to find the square root of a value
     function _sqrt(uint256 y) private pure returns (uint256 z) {
         if (y > 3) {
             z = y;
@@ -243,6 +215,7 @@ contract CPAMM {
         }
     }
 
+    /// function to find the minimum of both the values
     function _min(uint256 x, uint256 y) private pure returns (uint256) {
         return x <= y ? x : y;
     }
