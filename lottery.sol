7ce985c653c8b263361f24f1f06063d0199722d3
dhruv <dhruvagarwal2017@gmail.com>
2022-10-05 09:57:09 +0530

    Lottery contract added

---
+++ lottery.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Lottery is Ownable {
+    uint256 public fee = 0.01 ether;
+    address payable[] public players;
+    uint256 public lotteryId;
+    // from lottery Id to the winner;
+    mapping(uint256 => address payable) lotteryWinner;
+
+    enum State {
+        Started,
+        Completed,
+        WinnerPicked,
+        Closed
+    }
+
+    State public state = State.Closed;
+
+    event GameStarted();
+    event playerEntered(address _player, uint256 _fee);
+    event winnerPicked(address _winner, uint256 _amountWon);
+
+    function startLottery() public onlyOwner {
+        require(state = State.Closed, "Game Can't be started");
+        state = State.Started;
+        emit GameStarted();
+    }
+
+    function enterLottery() public payable {
+        require(state = State.Started, "Game not yet startead");
+        require(msg.value >= fee, "Invalid value sent");
+        players.push(msg.sender);
+        emit playerEntered(msg.sender, msg.value);
+    }
+
+    function pickWinner() external onlyOwner {
+        require(state != State.WinnerPicked, "Winne is already picked");
+        uint256 index = getRandomNumber() % players.length;
+        address winner = players[index];
+        lotteryWinner[lotteryId] = winner;
+        uint256 amount = address(this).balance;
+        state = State.WinnerPicked;
+        emit winnerPicked(winner, amount);
+        (bool success, ) = winner.call{value: amount}("");
+        require(success);
+        players = new address payable[](0);
+        lotteryId += 1;
+        state = State.Closed;
+    }
+
+    function getRandomNumber() internal view returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(owner, block.timestamp)));
+    }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+
+    function getLastWinner() public view retunrs(address) {
+        return lastWinner;
+    }
+}
